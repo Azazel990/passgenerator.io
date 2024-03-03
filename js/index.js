eaf3836c3d3c5348c4d49a41553fb69a3cb78458
Rohit <parabrohit161@gmail.com>
2024-03-03 20:40:38 +0530

    Digits Working

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const data = [0,1,2,3,4,5,6,7,8,9];
+
 $(document).ready(function(){
     $("#uppercase_box, #lowercase_box").hide();
 
@@ -5,4 +7,23 @@ $(document).ready(function(){
         $("#uppercase_box, #lowercase_box").toggle();
         $("#uppercase, #lowercase").prop("checked",true);
     })
+
+    $("#generateButton").on("click",function(){
+        const val_length = parseInt($("#val_length").val());
+        var password = "";
+        
+        for(let i = 0; i < val_length;i++){
+           password = password + data[generateRandomNumber(0,9)]; 
+        }
+
+        printPassword(password);
+    })
 })
+
+function printPassword(password){
+    $("#strong_password").val(password);
+}
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
