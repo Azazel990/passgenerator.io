a00e2d3f174ece77a752abfb9557a7b1fecc6977
Rohit <parabrohit161@gmail.com>
2024-03-03 20:47:02 +0530

    more changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const data = [0,1,2,3,4,5,6,7,8,9];
 
+const alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 $(document).ready(function(){
     $("#uppercase_box, #lowercase_box").hide();
 
@@ -11,7 +13,7 @@ $(document).ready(function(){
     $("#generateButton").on("click",function(){
         const val_length = parseInt($("#val_length").val());
         var password = "";
-        
+
         for(let i = 0; i < val_length;i++){
            password = password + data[generateRandomNumber(0,9)]; 
         }
@@ -25,5 +27,5 @@ function printPassword(password){
 }
 
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+   return Math.floor(Math.random() * (max - min + 1) ) + min;  
+}
